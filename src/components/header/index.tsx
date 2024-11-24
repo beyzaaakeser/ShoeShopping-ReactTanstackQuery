2565bc08df763d2838700cb346bbda6bd25a3d74
Beyza Keser <beyzaaakeser@gmail.com>
2024-11-24 14:49:18 +0300

    app & header updated

---
+++ src/components/header/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return <header className=''>
+    <nav>
+      <a>New Drops 🔥</a>
+      <a>Men</a>
+      <a>Women</a>
+    </nav>
+  </header>;
+};
+
+export default Header;
