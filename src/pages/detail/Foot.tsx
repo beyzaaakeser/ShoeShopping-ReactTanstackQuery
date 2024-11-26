db66692e4646d68d541ef1fca966fbb5c8ff7a4e
Beyza Keser <beyzaaakeser@gmail.com>
2024-11-26 20:59:18 +0300

    update

--- src/pages/detail/Foot.tsx
+++ src/pages/detail/Foot.tsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import { DetailProps } from '../../types'
+import React from 'react';
+import { DetailProps } from '../../types';
 
-const Foot = ({data} : DetailProps) => {
+const Foot = ({ data }: DetailProps) => {
+    console.log(data.description)
   return (
-    <div>Foot</div>
-  )
-}
+    <div>
+      <div className='flex flex-col gap-2 text-white'>
+        <div className='flex gap-2'>
+          <button className='flex-1 p-4 rounded-lg bg-black '>Add to Cart</button>
+          <button className='bg-black p-4 rounded-lg'>
+            <img src="/heart.svg" alt="" />
+          </button>
+        </div>
+        <button className='bg-blue p-4 rounded-lg'>Buy Now</button>
+      </div>
 
-export default Foot
+      <div>
+        <h2 className='font-semibold mt-8 mb-2 text-2xl text-gray-dark'>About This Product</h2>
+
+        <p className='font-open text-xl text-gray-dark'>
+            {data.description}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Foot;
--- src/pages/detail/Size.tsx
+++ src/pages/detail/Size.tsx
@@ -1,10 +1,45 @@
-import React from 'react'
-import { DetailProps } from '../../types'
+import React, { useState } from 'react';
+import { DetailProps } from '../../types';
+import { numbers } from '../../utils/constants';
 
-const Size = ({data} : DetailProps) => {
+const Size = ({ data }: DetailProps) => {
+  const [selected, setSelected] = useState<string>('');
+
+  const toggle = (size: string) => {
+    const isSame = selected === size;
+    if (isSame) {
+      setSelected('');
+    } else {
+      setSelected(size);
+    }
+  };
+
+  const stockSize = data.size.split(',');
   return (
-    <div>Size</div>
-  )
-}
+    <div>
+      <h2 className="mb-4 font-semibold">Choose Size</h2>
+      <div className="grid grid-cols-5 gap-4">
+        {numbers.map((num, index) => {
+          const found = selected.includes(num);
+          const inStock = stockSize.includes(num);
+          return (
+            <button
+              disabled={!inStock}
+              key={index}
+              type="button"
+              onClick={() => toggle(num)}
+              className={`py-2 px-4 lg:px-0 text-center rounded-md cursor-pointer transition
+              hover:bg-zinc-400 disabled:bg-[#D2D1D3] disabled:text-[#8F8C91] disabled:cursor-not-allowed ${
+                found ? 'bg-black text-white' : 'bg-white'
+              } `}
+            >
+              {num}
+            </button>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
-export default Size
+export default Size;
